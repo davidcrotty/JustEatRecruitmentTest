c2fb29d1f8420a497e425c2bc7d88d531d0173a6
David Crotty <david.crotty.work@gmail.com>
2024-02-04 13:45:10 +0000

    fetch resturants

--- app/src/main/java/com/example/justeatrecruitmenttest/PostCodeViewModel.kt
+++ app/src/main/java/com/example/justeatrecruitmenttest/PostCodeViewModel.kt
@@ -10,18 +10,32 @@ import kotlinx.coroutines.launch
 
 class PostCodeViewModel(private val repository: RestuarantRepository) : ViewModel() {
 
-    private val postCodePattern = """([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})"""
+    private val postCodePattern =
+        """([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})"""
 
     val uiState: StateFlow<PostCodeUIState>
         get() = _uiState
 
-    private val _uiState: MutableStateFlow<PostCodeUIState> = MutableStateFlow(PostCodeUIState.Initial)
+    private val _uiState: MutableStateFlow<PostCodeUIState> =
+        MutableStateFlow(PostCodeUIState.Initial)
 
     fun searchPostCode(postcode: PostCode) {
         viewModelScope.launch {
             if (postcode.text.matches(Regex(postCodePattern))) {
                 // API call
                 _uiState.value = PostCodeUIState.Loading
+                repository.getRestuarants().onSuccess { resturants ->
+                    val listItems = resturants.map {
+                      ResturantModel(
+                          name = it.name,
+                          rating = it.rating,
+                          typesOfFood = it.foodTypes.map { it.type }.reduce { acc, s -> "$acc, $s" }
+                      )
+                    }
+                    _uiState.value = PostCodeUIState.Success(listItems)
+                }.onFailure {
+                    _uiState.value = PostCodeUIState.Error("Unable to fetch resturants")
+                }
             } else {
                 _uiState.value = PostCodeUIState.Error("invalid Postcode")
             }
@@ -32,11 +46,11 @@ class PostCodeViewModel(private val repository: RestuarantRepository) : ViewMode
 
 sealed class PostCodeUIState {
 
-    object Initial: PostCodeUIState()
-    object Loading: PostCodeUIState()
-    class Error(val message: String): PostCodeUIState()
+    object Initial : PostCodeUIState()
+    object Loading : PostCodeUIState()
+    class Error(val message: String) : PostCodeUIState()
 
-    class Success(val result: ResturantModel): PostCodeUIState()
+    class Success(val result: List<ResturantModel>) : PostCodeUIState()
 }
 
 data class ResturantModel(val name: String, val rating: Int, val typesOfFood: String)

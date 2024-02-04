15a76d226ad1c4a07ad72a5c4492c87d7a7d89be
David Crotty <david.crotty.work@gmail.com>
2024-02-04 12:52:31 +0000

    postcode input

--- app/src/main/java/com/example/justeatrecruitmenttest/MainActivity.kt
+++ app/src/main/java/com/example/justeatrecruitmenttest/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.justeatrecruitmenttest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.justeatrecruitmenttest.ui.theme.JustEatRecruitmentTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PostCodeScreen()
                 }
             }
         }
@@ -30,17 +46,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun PostCodeScreen() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JustEatRecruitmentTestTheme {
-        Greeting("Android")
+    @Composable
+    fun PostCodeForm(modifier: Modifier, value: String, textChanged: (String) -> Unit) {
+        Box(Modifier.fillMaxWidth()) {
+            TextField(modifier = modifier.fillMaxWidth(), value = value, onValueChange = textChanged)
+        }
+    }
+
+    val postCode = remember { mutableStateOf("") }
+
+    Column(Modifier.padding(8.dp)) {
+        Text(modifier = Modifier.padding(8.dp), text = stringResource(id = R.string.postcode_label))
+        PostCodeForm(Modifier.padding(8.dp), postCode.value) {
+            postCode.value = it
+        }
     }
 }
+
+@Composable
+@Preview
+fun PostCodePreview() {
+    PostCodeScreen()
+}
+
